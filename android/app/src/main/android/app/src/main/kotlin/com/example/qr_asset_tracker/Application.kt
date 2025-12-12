608d6450d1117824e9174fca6e99718c020c689d
DFTYU1 <2485040265@qq.com>
2025-12-12 12:38:26 +0700

    Update Application.kt

--- android/app/src/main/android/app/src/main/kotlin/com/example/qr_asset_tracker/Application.kt
+++ android/app/src/main/android/app/src/main/kotlin/com/example/qr_asset_tracker/Application.kt
@@ -1,6 +1,21 @@
 package com.example.qr_asset_tracker
 
 import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val engine = FlutterEngine(this)
+        engine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", engine)
+    }
 }
